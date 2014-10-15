ad30035d647fbff5a0dfb3d824ab22498b8bb7d8
Martin Nowak <code@dawg.eu>
2014-10-15 21:53:11 +0200

    resolve circular reference
    - SSL_CTX is used in deimos.openssl.ssl where the
      definition of ssl_ctx_st is located too.
    
    - this broke with dmd 2.066.0

--- deimos/openssl/ossl_typ.d
+++ deimos/openssl/ossl_typ.d
@@ -204,7 +204,7 @@ alias st_ERR_FNS ERR_FNS;
 
 struct engine_st;
 alias engine_st ENGINE;
-import deimos.openssl.ssl;
+import deimos.openssl.ssl : ssl_st, ssl_ctx_st;
 alias ssl_st SSL;
 alias ssl_ctx_st SSL_CTX;
 
