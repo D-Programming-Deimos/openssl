D-Programming-Deimos/openssl

commit 90a74090250fdfd9bdd1b4aaf2acec493a893c66

Change array declarations from C style to D style to allow compile with latest dmd2 compiler.

deimos/openssl/bn.d
@@ -339,7 +339,7 @@ struct bn_mont_ctx_st
 	BIGNUM N;      /* The modulus */
 	BIGNUM Ni;     /* R*(1/R mod N) - N*Ni = 1
 	                * (Ni is only stored for bignum algorithm) */
-	BN_ULONG n0[2];/* least significant word(s) of Ni;
+	BN_ULONG[2] n0;/* least significant word(s) of Ni;
 	                  (type changed with 0.9.9, was "BN_ULONG n0;" before) */
 	int flags;
 	};
@@ -670,26 +670,24 @@ alias BN_ucmp BN_GF2m_cmp;
  *    t^p[0] + t^p[1] + ... + t^p[k]
  * where m = p[0] > p[1] > ... > p[k] = 0.
  */
-int	BN_GF2m_mod_arr(BIGNUM* r, const(BIGNUM)* a, const int p[]);
+int	BN_GF2m_mod_arr(BIGNUM* r, const(BIGNUM)* a, const int[] p);
 	/* r = a mod p */
 int	BN_GF2m_mod_mul_arr(BIGNUM* r, const(BIGNUM)* a, const(BIGNUM)* b,
-	const int p[], BN_CTX* ctx); /* r = (a* b) mod p */
-int	BN_GF2m_mod_sqr_arr(BIGNUM* r, const(BIGNUM)* a, const int p[],
+	const int[] p, BN_CTX* ctx); /* r = (a* b) mod p */
+int	BN_GF2m_mod_sqr_arr(BIGNUM* r, const(BIGNUM)* a, const int[] p,
 	BN_CTX* ctx); /* r = (a* a) mod p */
-int	BN_GF2m_mod_inv_arr(BIGNUM* r, const(BIGNUM)* b, const int p[],
+int	BN_GF2m_mod_inv_arr(BIGNUM* r, const(BIGNUM)* b, const int[] p,
 	BN_CTX* ctx); /* r = (1 / b) mod p */
 int	BN_GF2m_mod_div_arr(BIGNUM* r, const(BIGNUM)* a, const(BIGNUM)* b,
-	const int p[], BN_CTX* ctx); /* r = (a / b) mod p */
+	const int[] p, BN_CTX* ctx); /* r = (a / b) mod p */
 int	BN_GF2m_mod_exp_arr(BIGNUM* r, const(BIGNUM)* a, const(BIGNUM)* b,
-	const int p[], BN_CTX* ctx); /* r = (a ^ b) mod p */
+	const int[] p, BN_CTX* ctx); /* r = (a ^ b) mod p */
 int	BN_GF2m_mod_sqrt_arr(BIGNUM* r, const(BIGNUM)* a,
-	const int p[], BN_CTX* ctx); /* r = sqrt(a) mod p */
+	const int[] p, BN_CTX* ctx); /* r = sqrt(a) mod p */
 int	BN_GF2m_mod_solve_quad_arr(BIGNUM* r, const(BIGNUM)* a,
-	const int p[], BN_CTX* ctx); /* r^2 + r = a mod p */
-int	BN_GF2m_poly2arr(const(BIGNUM)* a, int p[], int max);
-int	BN_GF2m_arr2poly(const int p[], BIGNUM* a);
-
-}
+	const int[] p, BN_CTX* ctx); /* r^2 + r = a mod p */
+int	BN_GF2m_poly2arr(const(BIGNUM)* a, int[] p, int max);
+int	BN_GF2m_arr2poly(const int[] p, BIGNUM* a);
 
 /* faster mod functions for the 'NIST primes'
  * 0 <= a < p^2 */

deimos/openssl/dtls1.d
@@ -124,7 +124,7 @@ version(OPENSSL_NO_SCTP) {} else {
 struct dtls1_bitmap_st {
 	c_ulong map;		/* track 32 packets on 32-bit systems
 					   and 64 - on 64-bit systems */
-	ubyte max_seq_num[8];	/* max record number seen so far,
+	ubyte[8] max_seq_num;	/* max record number seen so far,
 					   64-bit value in big-endian
 					   encoding */
 	}
@@ -187,8 +187,8 @@ alias hm_fragment_st hm_fragment;
 
 struct dtls1_state_st {
 	uint send_cookie;
-	ubyte cookie[DTLS1_COOKIE_LENGTH];
-	ubyte rcvd_cookie[DTLS1_COOKIE_LENGTH];
+	ubyte[DTLS1_COOKIE_LENGTH] cookie;
+	ubyte[DTLS1_COOKIE_LENGTH] rcvd_cookie;
 	uint cookie_len;
 
 	/*
@@ -212,7 +212,7 @@ struct dtls1_state_st {
 	ushort handshake_read_seq;
 
 	/* save last sequence number for retransmissions */
-	ubyte last_write_sequence[8];
+	ubyte[8] last_write_sequence;
 
 	/* Received handshake records (processed and unprocessed) */
 	record_pqueue unprocessed_rcds;
@@ -249,9 +249,9 @@ struct dtls1_state_st {
 
 	/* storage for Alert/Handshake protocol data received but not
 	 * yet processed by ssl3_read_bytes: */
-	ubyte alert_fragment[DTLS1_AL_HEADER_LENGTH];
+	ubyte[DTLS1_AL_HEADER_LENGTH] alert_fragment;
 	uint alert_fragment_len;
-	ubyte handshake_fragment[DTLS1_HM_HEADER_LENGTH];
+	ubyte[DTLS1_HM_HEADER_LENGTH] handshake_fragment;
 	uint handshake_fragment_len;
 
 	uint retransmitting;

deimos/openssl/err.d
@@ -156,12 +156,12 @@ enum ERR_FLAG_MARK = 0x01;
 enum ERR_NUM_ERRORS = 16;
 struct err_state_st {
 	CRYPTO_THREADID tid;
-	int err_flags[ERR_NUM_ERRORS];
-	c_ulong err_buffer[ERR_NUM_ERRORS];
-	char* err_data[ERR_NUM_ERRORS];
-	int err_data_flags[ERR_NUM_ERRORS];
-	const(char)* err_file[ERR_NUM_ERRORS];
-	int err_line[ERR_NUM_ERRORS];
+	int[ERR_NUM_ERRORS] err_flags;
+	c_ulong[ERR_NUM_ERRORS] err_buffer;
+	char*[ERR_NUM_ERRORS] err_data;
+	int[ERR_NUM_ERRORS] err_data_flags;
+	const(char)*[ERR_NUM_ERRORS] err_file;
+	int[ERR_NUM_ERRORS] err_line;
 	int top,bottom;
 	}
 alias err_state_st ERR_STATE;
@@ -358,8 +358,8 @@ void ERR_print_errors(BIO* bp);
 }
 void ERR_add_error_data(int num, ...);
 void ERR_add_error_vdata(int num, va_list args);
-void ERR_load_strings(int lib,ERR_STRING_DATA str[]);
-void ERR_unload_strings(int lib,ERR_STRING_DATA str[]);
+void ERR_load_strings(int lib,ERR_STRING_DATA[] str);
+void ERR_unload_strings(int lib,ERR_STRING_DATA[] str);
 void ERR_load_ERR_strings();
 void ERR_load_crypto_strings();
 void ERR_free_strings();

deimos/openssl/evp.d
@@ -178,7 +178,7 @@ struct env_md_st
 	ExternC!(int function(int type, const(ubyte)* m, uint m_length,
 		      const(ubyte)* sigbuf, uint siglen,
 		      void* key)) verify;
-	int required_pkey_type[5]; /*EVP_PKEY_xxx */
+	int[5] required_pkey_type; /*EVP_PKEY_xxx */
 	int block_size;
 	int ctx_size; /* how big does the ctx->md_data need to be */
 	/* control function */
@@ -404,7 +404,7 @@ enum EVP_GCM_TLS_TAG_LEN = 16;
 
 struct evp_cipher_info_st {
 	const(EVP_CIPHER)* cipher;
-	ubyte iv[EVP_MAX_IV_LENGTH];
+	ubyte[EVP_MAX_IV_LENGTH] iv;
 	}
 alias evp_cipher_info_st EVP_CIPHER_INFO;
 
@@ -415,10 +415,10 @@ struct evp_cipher_ctx_st
 	int encrypt;		/* encrypt or decrypt */
 	int buf_len;		/* number we have left */
 
-	ubyte  oiv[EVP_MAX_IV_LENGTH];	/* original iv */
-	ubyte  iv[EVP_MAX_IV_LENGTH];	/* working iv */
-	ubyte buf[EVP_MAX_BLOCK_LENGTH];/* saved partial block */
-	int num;				/* used by cfb/ofb/ctr mode */
+	ubyte[EVP_MAX_IV_LENGTH]  oiv;	/* original iv */
+	ubyte[EVP_MAX_IV_LENGTH]  iv;	/* working iv */
+	ubyte[EVP_MAX_BLOCK_LENGTH] buf;/* saved partial block */
+	int num;				/* used by cfb/ofb mode */
 
 	void* app_data;		/* application stuff */
 	int key_len;		/* May change for variable length cipher */
@@ -436,7 +436,7 @@ struct evp_Encode_Ctx_st {
 			 * length that is ok.  Once decoding begins,
 			 * the length is adjusted up each time a longer
 			 * line is decoded */
-	ubyte enc_data[80];	/* data to encode */
+	ubyte[80] enc_data;	/* data to encode */
 	int line_num;	/* number read on current line */
 	int expect_nl;
 	}

deimos/openssl/hmac.d
@@ -78,7 +78,7 @@ struct hmac_ctx_st {
 	EVP_MD_CTX i_ctx;
 	EVP_MD_CTX o_ctx;
 	uint key_length;
-	ubyte key[HMAC_MAX_MD_CBLOCK];
+	ubyte[HMAC_MAX_MD_CBLOCK] key;
 	}
 alias hmac_ctx_st HMAC_CTX;
 

deimos/openssl/kssl.d
@@ -126,7 +126,7 @@ enum KSSL_CLOCKSKEW = 300;;
 enum KSSL_ERR_MAX = 255;
 struct kssl_err_st {
 	int  reason;
-	char text[KSSL_ERR_MAX+1];
+	char[KSSL_ERR_MAX+1] text;
 	}
 alias kssl_err_st KSSL_ERR;
 

deimos/openssl/pqueue.d
@@ -69,7 +69,7 @@ struct _pqueue;
 alias _pqueue* pqueue;
 
 struct _pitem {
-	ubyte priority[8]; /* 64-bit value in big-endian encoding */
+	ubyte[8] priority; /* 64-bit value in big-endian encoding */
 	void* data;
 	_pitem* next;
 	}

deimos/openssl/sha.d
@@ -108,7 +108,7 @@ struct SHAstate_st
 	{
 	SHA_LONG h0,h1,h2,h3,h4;
 	SHA_LONG Nl,Nh;
-	SHA_LONG data[SHA_LBLOCK];
+	SHA_LONG[SHA_LBLOCK] data;
 	uint num;
 	}
 alias SHAstate_st  SHA_CTX;
@@ -141,9 +141,9 @@ enum SHA224_DIGEST_LENGTH = 28;
 enum SHA256_DIGEST_LENGTH = 32;
 
 struct SHA256state_st {
-	SHA_LONG h[8];
+	SHA_LONG[8] h;
 	SHA_LONG Nl,Nh;
-	SHA_LONG data[SHA_LBLOCK];
+	SHA_LONG[SHA_LBLOCK] data;
 	uint num,md_len;
 	}
 alias SHA256state_st SHA256_CTX;
@@ -190,11 +190,11 @@ alias ulong SHA_LONG64;
 
 struct SHA512state_st
 	{
-	SHA_LONG64 h[8];
+	SHA_LONG64[8] h;
 	SHA_LONG64 Nl,Nh;
 	union u_ {
-		SHA_LONG64	d[SHA_LBLOCK];
-		ubyte	p[SHA512_CBLOCK];
+		SHA_LONG64[SHA_LBLOCK]	d;
+		ubyte[SHA512_CBLOCK]	p;
 	}
 	u_ u;
 	uint num,md_len;

commit c80f614db3fe8d999372ad7f92d0815be1316d54

Fix compilation
Fix syntax error introduced in commit
90a74090250fdfd9bdd1b4aaf2acec493a893c66.

deimos/openssl/bn.d
@@ -688,6 +688,7 @@ int	BN_GF2m_mod_solve_quad_arr(BIGNUM* r, const(BIGNUM)* a,
 	const int[] p, BN_CTX* ctx); /* r^2 + r = a mod p */
 int	BN_GF2m_poly2arr(const(BIGNUM)* a, int[] p, int max);
 int	BN_GF2m_arr2poly(const int[] p, BIGNUM* a);
+}
 
 /* faster mod functions for the 'NIST primes'
  * 0 <= a < p^2 */